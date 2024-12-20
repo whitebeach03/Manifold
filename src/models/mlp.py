bc329b8b7f92bc3934c58396e9ed79d9e7e828de
whitebeach03 <whitehama3@icloud.com>
2024-12-20 18:36:06 +0900

    add pca

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -10,14 +10,13 @@ import matplotlib.pyplot as plt
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(3*32*32, 2048)
-        self.fc2 = nn.Linear(2048, 1024)
-        self.fc3 = nn.Linear(1024, 512)
-        self.fc4 = nn.Linear(512, 256)
-        self.fc5 = nn.Linear(256, 128)
-        self.fc6 = nn.Linear(128, 64)
-        self.fc7 = nn.Linear(64, 32)
-        self.fc8 = nn.Linear(32, 10)
+        self.fc1 = nn.Linear(3*32*32, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, 64)
+        self.fc6 = nn.Linear(64, 32)
+        self.fc7 = nn.Linear(32, 10)
 
     def forward(self, x): 
         x = F.relu(self.fc1(x))
@@ -26,8 +25,17 @@ class MLP(nn.Module):
         x = F.relu(self.fc4(x))
         x = F.relu(self.fc5(x))
         x = F.relu(self.fc6(x))
-        x = F.relu(self.fc7(x))
-        print(x.shape)
-        x = self.fc8(x)
+        # print(x.shape) # (128, 32) ... (batch_size, 次元)
+        x = self.fc7(x)
         return x
 
+
+# Number of parameters
+model = MLP()
+
+def count_parameters(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    return total_params
+
+total_params = count_parameters(model)
+print(f"Number of parameters: {total_params}")
