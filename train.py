30a4955f32db9352936c2d5491fb69048f9137ec
shirahama <shirahama@synapse.cs.tsukuba.ac.jp>
2025-01-09 14:10:46 +0900

    transfer to synapse

--- train.py
+++ train.py
@@ -13,70 +13,70 @@ from torch.utils.data import random_split, DataLoader, Dataset
 from tqdm import tqdm
 from src.models.mlp import MLP
 from src.models.cnn import SimpleCNN
+from src.models.resnet import *
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 
 i = 0
 
 def main():
-    args = parse_args()
-    epochs = args.epochs
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
+    parser.add_argument("--data_type", type=str, choices=["mnist", "cifar10", "stl10"], default="stl10", help="Dataset to use")
+    args = parser.parse_args() 
+
+    epochs    = args.epochs
     data_type = args.data_type
+
     batch_size = 128
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = SimpleCNN().to(device)
+    device     = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model      = SimpleCNN().to(device)
 
-    # # データセットの選択
-    # if data_type == 'mnist':
-    #     transform = transforms.ToTensor()
-    #     train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-    #     test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transform, download=True)
-    # elif data_type == 'cifar10':
-    #     transform = transforms.Compose([
-    #         transforms.ToTensor(),
-    #         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    #     ])
-    #     train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
-    #     test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
-    # elif data_type == 'stl10':
-    #     transform = transforms.Compose([
-    #         transforms.Grayscale(num_output_channels=1),
-    #         transforms.ToTensor()
-    #     ])
-    #     train_dataset = torchvision.datasets.STL10(root='./data', split='train', transform=transform, download=True)
-    #     test_dataset = torchvision.datasets.STL10(root='./data', split='test', transform=transform, download=True)
+    # データセットの選択
+    if data_type == 'mnist':
+        transform     = transforms.ToTensor()
+        train_dataset = torchvision.datasets.MNIST(root='./data', train=True,  transform=transform, download=True)
+        test_dataset  = torchvision.datasets.MNIST(root='./data', train=False, transform=transform, download=True)
+    elif data_type == 'cifar10':
+        transform     = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,  transform=transform, download=True)
+        test_dataset  = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
+    elif data_type == 'stl10':
+        transform     = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.ToTensor()])
+        train_dataset = torchvision.datasets.STL10(root='./data', split='train', transform=transform, download=True)
+        test_dataset  = torchvision.datasets.STL10(root='./data', split='test',  transform=transform, download=True)
     
-    # # トレーニング・検証データの分割
-    # n_samples = len(train_dataset)
-    # n_train = int(n_samples * 0.8)
-    # n_val = n_samples - n_train
-    # train_dataset, val_dataset = random_split(train_dataset, [n_train, n_val])
+    # トレーニング・検証データの分割
+    n_samples = len(train_dataset)
+    n_train   = int(n_samples * 0.8)
+    n_val     = n_samples - n_train
 
-    # train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-    # val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
-    # test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+######################################################################################################################################################################
+
+    train_dataset, val_dataset = random_split(train_dataset, [n_train, n_val])
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader   = DataLoader(dataset=val_dataset,   batch_size=batch_size, shuffle=False)
+    test_loader  = DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False)
+    for batch in train_loader:
+        images, labels = batch
+        print(images.shape)
+        print(labels.shape)
+        break
     
 ######################################################################################################################################################################
-    # 検証・テストデータの読み込み（元のSTL10データ）
-    transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.ToTensor()])
-    train_dataset_original = torchvision.datasets.STL10(root='./data', split='train', transform=transform, download=True)
-    test_dataset = torchvision.datasets.STL10(root='./data', split='test', transform=transform, download=True)
 
-    # 検証データの分割
-    n_samples = len(train_dataset_original)
-    n_train = int(n_samples * 0.8)
-    n_val = n_samples - n_train
-    _, val_dataset = random_split(train_dataset_original, [n_train, n_val])
+    # _, val_dataset = random_split(train_dataset, [n_train, n_val])
 
-    val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    # val_loader  = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+    # test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-    # 訓練データの読み込み（生成データ）
-    generated_data = np.load('all_generated_high_dim_data.npy')
-    labels = np.concatenate([np.full(500, i) for i in range(10)])  # 各クラス500サンプル
+    # # 訓練データの読み込み（生成データ）
+    # generated_data = np.load('all_generated_high_dim_data.npy')
+    # labels = np.concatenate([np.full(500, i) for i in range(10)])  # 各クラス500サンプル
 
-    train_dataset_generated = GeneratedDataset(generated_data, labels)
-    train_loader = DataLoader(dataset=train_dataset_generated, batch_size=batch_size, shuffle=True)
+    # train_dataset_generated = GeneratedDataset(generated_data, labels)
+    # train_loader = DataLoader(dataset=train_dataset_generated, batch_size=batch_size, shuffle=True)
+
 ######################################################################################################################################################################
 
     criterion = nn.CrossEntropyLoss()
@@ -87,10 +87,10 @@ def main():
     os.makedirs('./logs/normal', exist_ok=True)
     os.makedirs('./history/normal', exist_ok=True)
 
-    # モデルのトレーニング
+    # Train #
     for epoch in range(epochs):
         train_loss, train_acc = train(model, train_loader, criterion, optimizer, device)
-        val_loss, val_acc = val(model, val_loader, criterion, device)
+        val_loss, val_acc     = val(model, val_loader, criterion, device)
 
         if score <= val_acc:
             print('Saving model parameters...')
@@ -107,7 +107,7 @@ def main():
     with open(f'./history/normal_{epochs}.pickle', 'wb') as f:
         pickle.dump(history, f)
 
-    # テストセットで評価
+    # Test #
     model.load_state_dict(torch.load(model_save_path))
     model.eval()
     test_loss, test_acc = test(model, test_loader, criterion, device)
@@ -138,7 +138,6 @@ def train(model, train_loader, criterion, optimizer, device):
     train_acc /= len(train_loader)
     return train_loss, train_acc
 
-
 def val(model, val_loader, criterion, device):
     model.eval()
     val_loss = 0.0
@@ -156,7 +155,6 @@ def val(model, val_loader, criterion, device):
     val_acc /= len(val_loader)
     return val_loss, val_acc
 
-
 def test(model, test_loader, criterion, device):
     model.eval()
     test_loss = 0.0
@@ -173,14 +171,6 @@ def test(model, test_loader, criterion, device):
     test_loss /= len(test_loader)
     test_acc /= len(test_loader)
     return test_loss, test_acc
-
-
-def parse_args():
-    """コマンドライン引数を処理"""
-    parser = argparse.ArgumentParser(description="Train a model with CIFAR-10 or MNIST")
-    parser.add_argument("--epochs", type=int, default=20, help="Number of training epochs")
-    parser.add_argument("--data_type", type=str, choices=["mnist", "cifar10", "stl10"], default="stl10", help="Dataset to use")
-    return parser.parse_args()
 
 class GeneratedDataset(Dataset):
     """生成データ用データセットクラス"""
