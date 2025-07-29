c14ca80e4d17f17981a3d38afd991aa10566c210
whitebeach03 <whitehama3@icloud.com>
2025-07-29 19:20:19 +0900

    seminar

--- test.py
+++ test.py
@@ -9,111 +9,6 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import STL10, CIFAR10, CIFAR100
 from src.utils import test
 from tqdm import tqdm
-
-# class IndexedDataset(Dataset):
-#     def __init__(self, base_dataset):
-#         self.dataset = base_dataset
-
-#     def __len__(self):
-#         return len(self.dataset)
-
-#     def __getitem__(self, idx):
-#         img, label = self.dataset[idx]
-#         return img, label, idx
-
-# def test_and_collect_errors(model, loader, criterion, device):
-#     model.eval()
-#     total_loss = 0.0
-#     total_correct = 0
-#     total_samples = 0
-#     errors = []   # 誤分類リスト。sample_idx, true, pred を格納
-
-#     with torch.no_grad():
-#         for inputs, targets, indices in tqdm(loader, leave=False):
-#             inputs, targets = inputs.to(device), targets.to(device)
-#             outputs = model(inputs, targets, device=device, augment="Mixup")
-#             total_loss += criterion(outputs, targets).item() * inputs.size(0)
-#             _, preds = torch.max(outputs, dim=1)
-
-#             total_correct += (preds == targets).sum().item()
-#             total_samples += targets.size(0)
-
-#             # 誤分類をチェック
-#             mismatch = preds != targets
-#             for i in torch.where(mismatch)[0]:
-#                 errors.append({
-#                     "index": indices[i].item(),
-#                     "true_label": targets[i].item(),
-#                     "pred_label": preds[i].item()
-#                 })
-
-#     avg_loss = total_loss / total_samples
-#     accuracy = total_correct / total_samples
-#     return avg_loss, accuracy, errors
-
-# class_names = [
-#     "airplane",
-#     "bird",
-#     "car",
-#     "cat",
-#     "deer",
-#     "dog",
-#     "horse",
-#     "monkey",
-#     "ship",
-#     "truck",
-# ]
-
-# # --- 準備 ---
-# transform = transforms.Compose([
-#     transforms.ToTensor(), 
-#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-# ])
-# # test_dataset = CIFAR10(root="./data", train=False, transform=transform, download=True)
-# test_dataset = STL10(root="./data", split="train", download=True, transform=transform)
-# indexed_test = IndexedDataset(test_dataset)
-# test_loader = DataLoader(indexed_test, batch_size=128, shuffle=False)
-
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# # model = Wide_ResNet(28, 10, 0.3, num_classes=10).to(device)
-# model = ResNet18().to(device)
-# # model_save_path = "./logs/wide_resnet_28_10/Mixup/cifar10_250_0.pth"
-# model_save_path = "./logs/resnet18/Mixup/stl10_200.pth"
-# criterion = nn.CrossEntropyLoss()
-
-# model.load_state_dict(torch.load(model_save_path, weights_only=True))
-# test_loss, test_acc, misclassified = test_and_collect_errors(model, test_loader, criterion, device)
-
-# print(f"Loss={test_loss:.4f}  Acc={test_acc:.4f}  Errors={len(misclassified)} samples")
-# # 最初の数件を表示
-# for e in misclassified[:10]:
-#     # print(f"  idx={e['index']}  true={e['true_label']}  pred={e['pred_label']}")
-#     true_name = class_names[e['true_label']]
-#     pred_name = class_names[e['pred_label']]
-#     print(f"idx={e['index']}  true={true_name}  pred={pred_name}")
-
-
-# # 誤分類サンプルの先頭16件
-# samples = misclassified[:16]
-
-# fig, axes = plt.subplots(4, 4, figsize=(8,8))
-# for ax, s in zip(axes.flatten(), samples):
-#     img, _ = test_dataset[s["index"]]     # transform 後の Tensor
-#     img = img.numpy().transpose(1,2,0)
-#     img = np.clip(img * np.array([0.229,0.224,0.225]) + np.array([0.485,0.456,0.406]), 0, 1)
-#     ax.imshow(img)
-
-#     # ← ここでその都度ラベル名を取り出す
-#     true_name = class_names[s["true_label"]]
-#     pred_name = class_names[s["pred_label"]]
-#     ax.set_title(f"T: {true_name} / P: {pred_name}")
-
-#     ax.axis("off")
-# plt.tight_layout()
-# plt.savefig("error_samples_STL10.png")
-# plt.show()
-
-
 import argparse
 import numpy as np
 import os
@@ -133,13 +28,40 @@ from tqdm import tqdm
 from torchvision.datasets import STL10, CIFAR10, CIFAR100
 from torch.utils.data import DataLoader, random_split, Subset
 from src.methods.foma import foma
-from batch_sampler import extract_wrn_features, FeatureKNNBatchSampler, HybridFOMABatchSampler
+
+class CIFAR100C(Dataset):
+    def __init__(self, corruption_type, severity=1, root='./data/CIFAR-100-C', transform=None):
+        self.data = np.load(f"{root}/{corruption_type}.npy")
+        self.labels = np.load(f"{root}/labels.npy")
+        assert 1 <= severity <= 5
+        self.data = self.data[(severity - 1) * 10000 : severity * 10000]
+        self.labels = self.labels[(severity - 1) * 10000 : severity * 10000]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img = self.data[idx]
+        label = self.labels[idx]
+        img = img.astype(np.uint8)
+        if self.transform:
+            img = self.transform(img)
+        return img, label
 
 augmentations = [
-    # "Default",
+    "Default",
     "Mixup",
     # "Manifold-Mixup",
-    # "Local-FOMA",
+    "Local-FOMA",
+    "FOMA-Mixup"
+]
+
+corruption_types = [
+    'gaussian_noise', 'shot_noise', 'impulse_noise',
+    'defocus_blur', 'glass_blur', 'motion_blur', 'zoom_blur',
+    'snow', 'frost', 'fog', 'brightness',
+    'contrast', 'elastic_transform', 'pixelate', 'jpeg_compression'
 ]
 
 def main():
@@ -188,8 +110,15 @@ def main():
             test_dataset = CIFAR10(root="./data", train=False, transform=transform, download=True)
 
         test_loader  = DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False)
+
+        # corruption = 'gaussian_noise'  # 他にも 'fog', 'motion_blur' など
+        # severity = 5
+        # test_dataset_C = CIFAR100C(corruption_type=corruption, severity=severity, transform=transform)
+        # test_loader_C = torch.utils.data.DataLoader(test_dataset_C, batch_size=128, shuffle=False)
         
         for augment in augmentations:
+            total_acc = 0
+            total_loss = 0
             print(f"\n==> Test with {augment} ...")
 
             # Select Model
@@ -204,8 +133,22 @@ def main():
 
             model_save_path = f"./logs/{model_type}/{augment}/{data_type}_{epochs}_{i}.pth"
             model.load_state_dict(torch.load(model_save_path, weights_only=True))
-            test_loss, test_acc = test(model, test_loader, criterion, device, augment, aug_ok=False)
-            print(f"Test Loss: {test_loss:.3f}, Test Accuracy: {test_acc:.3f}")
+            # test_loss, test_acc = test(model, test_loader, criterion, device, augment, aug_ok=False)
+            # print(f"[CIFAR-100]   Test Loss: {test_loss:.3f}, Test Accuracy: {test_acc:.3f}")
+
+            for corruption in corruption_types:
+                test_dataset_C = CIFAR100C(corruption_type=corruption, severity=1, transform=transform)
+                test_loader_C = torch.utils.data.DataLoader(test_dataset_C, batch_size=256, shuffle=False)
+
+                test_loss_C, test_acc_C = test(model, test_loader_C, criterion, device, augment, aug_ok=False)
+                print(f"  [{corruption}] Loss: {test_loss_C:.3f}, Accuracy: {test_acc_C:.2f}%")
+                total_acc += test_acc_C
+                total_loss += test_loss_C
+
+            avg_acc = total_acc / len(corruption_types)
+            avg_loss = total_loss / len(corruption_types)
+
+            print(f"\n[CIFAR-100-C Average] Loss: {avg_loss:.4f}, Accuracy: {avg_acc:.4f}%")
 
 if __name__ == "__main__":
     main()
